ffa179d24ac76bc907427cc58f8de5402e6dee3f
Md. Rishad Hossain <82581808+voidrishad11@users.noreply.github.com>
2023-01-14 13:20:32 +0600

    Update 5_c_f.c

--- Assignment_01/5_c_f.c
+++ Assignment_01/5_c_f.c
@@ -9,5 +9,5 @@ void main()
     printf("Enter current temperature in Celsius\n");
     scanf("%f",&c);
     f= (1.8*c) + 32;
-    printf("The %.2f degree Celcius is equivalent to %.2f degree Fahrenheit\n",c,f);
+    printf("The %.2f degree Celsius is equivalent to %.2f degree Fahrenheit\n",c,f);
 }
